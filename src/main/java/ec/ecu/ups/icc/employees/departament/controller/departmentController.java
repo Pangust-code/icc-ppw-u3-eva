2554db0a2e688460339651948c65b69f78cd78ed
Pangust-code <www.daniku134@gmail.com>
2026-02-03 18:44:28 -0500

    add new content

--- src/main/java/ec/ecu/ups/icc/employees/departament/controller/departmentController.java
+++ src/main/java/ec/ecu/ups/icc/employees/departament/controller/departmentController.java
@@ -1,6 +1,5 @@
 package ec.ecu.ups.icc.employees.departament.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- src/main/java/ec/ecu/ups/icc/employees/employee/controller/EmployeeController.java
+++ src/main/java/ec/ecu/ups/icc/employees/employee/controller/EmployeeController.java
@@ -1,6 +1,5 @@
 package ec.ecu.ups.icc.employees.employee.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
